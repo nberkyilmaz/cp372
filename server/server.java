3ee58735418b35adb47982c13551d6eab6e91139
marcusalunni <91908331+marcusalunni@users.noreply.github.com>
2021-10-05 13:55:35 -0400

    Update server.java

--- server/server.java
+++ server/server.java
@@ -5,11 +5,13 @@ import javax.swing.*;
 
 public final class server{
     public static void main(String argv[]) throws Exception {
+        //initializing values
         int port = 0;
         int b_width = 0;
         int b_height = 0;
         String colour[] = new String[argv.length - 3];
-        //taking inout from client
+
+        //taking input from client/ error handle
         if (argv.length < 4){
             System.out.println("input not valid");
             System.out.println("Enter inputs in form: ");
@@ -18,11 +20,9 @@ public final class server{
             port = Integer.valueOf(argv[0]);
             b_width = Integer.valueOf(argv[1]);
             b_height = Integer.valueOf(argv[2]);
+            //adding colours to colour array
             for(int i = 3; i < argv.length; i++){
-                System.out.println("" + i);
-                System.out.println(String.valueOf(argv[i]));
                 colour[i - 3] = String.valueOf(argv[i]);
-                //colour = String.valueOf(argv[3]);
             }
             
         }
@@ -31,22 +31,36 @@ public final class server{
 
         ServerSocket ss = new ServerSocket(port);
         Socket s = ss.accept();
-        //int port;
-        //port = Integer.valueOf(argv[0]);
-        //port = 4554;
+        InetAddress ip;
+        ip = InetAddress.getLocalHost();
+
+        //displaying what we know
         System.out.println("length: " + argv.length);
         System.out.println("Connected");
         System.out.println("port: "+ port);
         System.out.println("B_width: " + b_width);
         System.out.println("B_height: " + b_height);
-        System.out.println("Colour: " + colour);
+        System.out.println("ip: " + ip);
+        System.out.println("Colour(s): ");
         for (int i = 0; i<=argv.length-4; i++){
-            System.out.print(colour[i]);
+            System.out.print(colour[i] + ", ");
         } 
 
+        //GUI
         JFrame frame = new JFrame("BBoard");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(b_width,b_height);
+
+        //JButton button1 = new JButton("POST");
+        //JButton button2 = new JButton("GET");
+        //frame.getContentPane().add(button1);
+        //frame.getContentPane().add(button2);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter Text:");
+        JTextField tf = new JTextField(20); // accepts upto 20 characters
+        JButton send = new JButton("GET");
+        JButton reset = new JButton("POST");
+
         frame.setVisible(true);
         
     }    
